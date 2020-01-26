ea430f87f3d2b140d0fe897faf207becbc50c51e
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-26 00:54:09 -0800

    cleanp

--- src/sorter.ts
+++ src/sorter.ts
@@ -1,19 +1,18 @@
-import {NumbersCollection} from './NumbersCollection'
+import { NumbersCollection } from './NumbersCollection';
 
 export class Sorter {
-    //when you use or operator you can only access the properties that are common between number and string
-    constructor(public collection: NumbersCollection){}
+  //when you use or operator you can only access the properties that are common between number and string
+  constructor(public collection: NumbersCollection) {}
 
-    sort(): void{
-        const {length} = this.collection
-        //bubble sort
-        for(let i = 0; i< length; i++){
-            for(let j = 0; j< length -i -1; j++){
-                if(this.collection.compare(j, j+1)){
-                    this.collection.swap(j, j+1)
-                } 
-            }
+  sort(): void {
+    const { length } = this.collection;
+    //bubble sort
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
+      }
     }
+  }
 }
-
