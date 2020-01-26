95d67f990d8fe6338b88b3b50aafcbc275e67bf2
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-26 09:57:51 -0800

    implement linked list

--- build/index.js
+++ build/index.js
@@ -3,13 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 //sort array of numbers
 var numbersCollection = new NumbersCollection_1.NumbersCollection([-5000, 100000, 1000, 10, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+console.log('---------------------------');
 //sort string
 var charactersCollection = new CharactersCollection_1.CharactersCollection('tylerGasperling');
 var stringSorter = new Sorter_1.Sorter(charactersCollection);
 stringSorter.sort();
 console.log(charactersCollection.data);
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(5000);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(100);
+console.log('---------------------------');
+var sorter3 = new Sorter_1.Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection'
+import {LinkedList} from './LinkedList'
 
 //sort array of numbers
 const numbersCollection = new NumbersCollection([-5000,100000,1000,10,3,-5,0])
@@ -8,8 +9,22 @@ const sorter = new Sorter(numbersCollection)
 sorter.sort();
 console.log(numbersCollection.data)
 
+console.log('---------------------------')
+ 
 //sort string
 const charactersCollection = new CharactersCollection('tylerGasperling')
 const stringSorter = new Sorter(charactersCollection)
 stringSorter.sort()
 console.log(charactersCollection.data)
+
+const linkedList = new LinkedList()
+linkedList.add(5000)
+linkedList.add(-10);
+linkedList.add(-3)
+linkedList.add(100)
+
+console.log('---------------------------')
+
+const sorter3 = new Sorter(linkedList)
+sorter3.sort()
+linkedList.print()
