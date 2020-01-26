5faf6c11284966ec814b4b8e00d26781b4684a2b
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-26 07:16:01 -0800

    implement sorter for string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
- 
+import {CharactersCollection} from './CharactersCollection'
+
 const numbersCollection = new NumbersCollection([-5000,100000,1000,10,3,-5,0])
 const sorter = new Sorter(numbersCollection)
 sorter.sort();
 console.log(numbersCollection.data)
+
+
+const charactersCollection = new CharactersCollection('tylerGasperling')
+const stringSorter = new Sorter(charactersCollection)
+stringSorter.sort()
+console.log(charactersCollection.data)
