38fdce218e5577e88759dc781e03d1d268e8f735
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-26 07:40:30 -0800

    create linked list

--- build/index.js
+++ build/index.js
@@ -3,10 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+//sort array of numbers
 var numbersCollection = new NumbersCollection_1.NumbersCollection([-5000, 100000, 1000, 10, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+//sort string
 var charactersCollection = new CharactersCollection_1.CharactersCollection('tylerGasperling');
 var stringSorter = new Sorter_1.Sorter(charactersCollection);
 stringSorter.sort();
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import {Sorter} from './Sorter';
 import {NumbersCollection} from './NumbersCollection';
 import {CharactersCollection} from './CharactersCollection'
 
+//sort array of numbers
 const numbersCollection = new NumbersCollection([-5000,100000,1000,10,3,-5,0])
 const sorter = new Sorter(numbersCollection)
 sorter.sort();
 console.log(numbersCollection.data)
 
-
+//sort string
 const charactersCollection = new CharactersCollection('tylerGasperling')
 const stringSorter = new Sorter(charactersCollection)
 stringSorter.sort()
